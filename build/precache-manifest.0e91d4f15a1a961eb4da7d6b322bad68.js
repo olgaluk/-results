7eaf9380ad4b726e9fd5cba46a669f9c3d8e8fdb
Olga Luk <olga_ivanova_86@mail.ru>
2019-02-27 18:05:41 +0300

    feat: add realization

---
+++ build/precache-manifest.0e91d4f15a1a961eb4da7d6b322bad68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "3a4e944a629522df4de9",
+    "url": "/static/js/main.3a4e944a.chunk.js"
+  },
+  {
+    "revision": "8e616af1211142e24027",
+    "url": "/static/js/2.8e616af1.chunk.js"
+  },
+  {
+    "revision": "3a4e944a629522df4de9",
+    "url": "/static/css/main.f5461a65.chunk.css"
+  },
+  {
+    "revision": "c1906d578f0c6c8fdcb90b779ae0aaa0",
+    "url": "/index.html"
+  }
+];
